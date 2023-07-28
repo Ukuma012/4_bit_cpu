b91a6d3dea5fe0f593fc5e6770eaef34ce56fdce
Ukuma012 <Ukuma012@gmail.com>
2023-07-28 22:27:37 +0900

    cpu

---
+++ main/mother_board/cpu.sv
@@ -0,0 +1,14 @@
+`include "lib_cpu_svh"
+
+module cpu import lib_cpu :: *; (
+    ctrl_bus_if.master ctrl_bus,
+    io_bus_if.master io_bus,
+    mem_bus_if.master mem_bus
+);
+    OPECODE opecode;
+    logic [7:0] imm;
+    decoder decoder(.data(mem_bus.data), .opecode, .imm);
+
+    REGS current, next;
+    assign mem_bus.addr = current.ip;
+    assign io_bus.led = current.out;
---
+++ main/package/lib_cpu.svh
@@ -0,0 +1,25 @@
+`ifndef LIB_CPU_SVH
+`define LIB_CPU_SVH
+    package lib_cpu;
+        typedef struct packed {
+            logic [3:0] a, b, ip, out;
+            logic cf;
+        } REGS;
+
+    typedef enum logic [7:0] {
+        ADD_A_IMM,
+        ADD_B_IMM,
+        MOV_A_IMM,
+        MOV_B_IMM,
+        MOV_A_B,
+        MOV_B_A,
+        JMP_IMM,
+        JNC_IMM,
+        IN_A,
+        IN_B,
+        OUT_B,
+        OUT_IMM,
+        INVALID
+    } OPECODE;
+endpackage
+`endif
