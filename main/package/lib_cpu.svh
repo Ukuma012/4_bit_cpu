fcb2f9b529ee03c473c655fcc444f5d55be8a688
Ukuma012 <Ukuma012@gmail.com>
2023-07-29 13:27:17 +0900

    opecode

--- main/package/lib_cpu.svh
+++ main/package/lib_cpu.svh
@@ -7,19 +7,19 @@
         } REGS;
 
     typedef enum logic [7:0] {
-        ADD_A_IMM,
-        ADD_B_IMM,
-        MOV_A_IMM,
-        MOV_B_IMM,
-        MOV_A_B,
-        MOV_B_A,
-        JMP_IMM,
-        JNC_IMM,
-        IN_A,
-        IN_B,
-        OUT_B,
-        OUT_IMM,
-        INVALID
+        MOV_A_B,        //4'b0000
+        MOV_B_A,        //4'b0001
+        MOV_A_IMM,      //4'b0010
+        MOV_B_IMM,      //4'b0011
+        IN_A,           //4'b0100
+        IN_B,           //4'b0101
+        OUT_B,          //4'b0110
+        OUT_IMM,        //4'b0111
+        ADD_A_IMM,      //4'b1000
+        ADD_B_IMM,      //4'b1001
+        JMP_IMM,        //4'b1010
+        JNC_IMM,        //4'b1011
+        INVALID         //4'b1100
     } OPECODE;
 endpackage
 `endif
