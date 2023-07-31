2d73f2e2567bdc4294a2ab3f20cc135c039a55e7
Ukuma012 <Ukuma012@gmail.com>
2023-07-31 13:38:29 +0900

    operation library

--- main/package/lib_operation.svh
+++ main/package/lib_operation.svh
@@ -15,10 +15,60 @@ package lib_operation;
         mov_a_b.a = current.b;
     endfunction
 
+    function automatic REGS mov_b_a(REGS current);
+        mov_b_a = nop(current);
+        mov_b_a.b = current.a;
+    endfunction
 
     function automatic REGS mov_a_imm(REGS current, logic [3:0] imm);
         mov_a_imm = nop(current);
         mov_a_imm.a = imm;
     endfunction
 
+    function automatic REGS mov_b_imm(REGS current, logic [3:0] imm);
+        mov_b_imm = nop(current);
+        mov_b_imm.b = imm;
+    endfunction
+
+    function automatic REGS in_a(REGS current, logic [3:0] switch);
+        in_a = nop(current);
+        in_a.a = switch;
+    endfunction
+
+    function automatic REGS in_b(REGS current, logic [3:0] switch);
+        in_b = nop(current);
+        in_b.b = switch;
+    endfunction
+
+    function automatic REGS out_b(REGS current);
+        out_b = nop(current);
+        out_b.out = current.b;
+    endfunction
+
+    function automatic REGS out_imm(REGS current, logic [3:0] imm);
+        out_imm = nop(current);
+        out_imm.out = imm; 
+    endfunction
+
+    function automatic REGS add_a_imm(REGS current, logic [3:0] imm);
+        add_a_imm = nop(current);
+        {add_a_imm.cf, add_a_imm.a} = current.a + imm;
+    endfunction
+
+    function automatic REGS add_b_imm(REGS current, logic [3:0] imm);
+        add_b_imm = nop(current);
+        {add_b_imm.cf, add_b_imm.b} = current.b + imm;
+    endfunction
+
+    function automatic REGS jmp_imm(REGS current, logic [3:0] imm);
+        jmp_imm = nop(current);
+        jmp_imm.ip = imm; 
+    endfunction
+
+    function automatic REGS jnc_imm(REGS current, logic [3:0] imm);
+        jnc_imm = nop(current);
+        jnc_imm.ip = current.cf ? current.ip + 4'd1 : imm;
+    endfunction
+
+endpackage
 `endif
